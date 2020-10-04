a1bb5b517cdf9ab24baaa5f4d57b9f2e20c12327
Nathaniel Starkman <nstarkman@protonmail.com>
2020-10-04 19:21:19 -0400

    apply template to init
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,12 +1,39 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+# see LICENSE.rst
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : {{ cookiecutter.package_name }}
+# AUTHOR  : {{ cookiecutter.author_name }}
+# PROJECT : {{ cookiecutter.package_name }}
+#
+# ----------------------------------------------------------------------------
+
+"""{{ cookiecutter.package_name }}."""
+
+__author__ = "{{ cookiecutter.author_name }}"
+# __copyright__ = "Copyright 2018, "
+# __credits__ = [""]
+# __license__ = ""
+# __version__ = "0.0.0"
+# __maintainer__ = ""
+# __email__ = ""
+# __status__ = "Production"
+
+__all__ = [
+    # modules
+    # functions
+]
+
+
+##############################################################################
+# IMPORTS
 
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._{{ cookiecutter._parent_project }}_init import *   # noqa
 # ----------------------------------------------------------------------------
-
-__all__ = []
 
 {%- if cookiecutter.include_example_code == 'y' %}
 from .example_mod import *   # noqa
@@ -15,3 +42,7 @@ __all__ += ['do_primes']   # noqa
 # or you can keep everything from the subpackage with the following instead
 # __all__ += example_mod.__all__
 {%- endif %}
+
+
+##############################################################################
+# END
