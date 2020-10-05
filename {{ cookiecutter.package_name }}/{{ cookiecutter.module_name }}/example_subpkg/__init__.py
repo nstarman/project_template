6e3fd539d5a50a20745e3a9d36485bc23d95c7f6
Nathaniel Starkman <nstarkman@protonmail.com>
2020-10-05 00:52:34 -0400

    example_subpkg init
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/example_subpkg/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/example_subpkg/__init__.py
@@ -1,5 +1,29 @@
-"""
+# -*- coding: utf-8 -*-
+# see LICENSE.rst
+
+"""Example Subpackage.
+
 This is the docstring for the examplesubpkg package.  Normally you would
 have whatever.py files in this directory implementing some modules, but this
 is just an example sub-package, so it doesn't actually do anything.
+
 """
+
+__author__ = "{{ cookiecutter.author_name }}"
+# __copyright__ = "Copyright 2020, "
+# __credits__ = [""]
+# __maintainer__ = ""
+# __email__ = ""
+
+
+# __all__ = [
+#     ""
+# ]
+
+
+##############################################################################
+# IMPORTS
+
+
+##############################################################################
+# END
