0ec77c7c28e8aec1b799c45166ecbeaba7666f54
Nathaniel Starkman <nstarkman@protonmail.com>
2020-10-05 00:54:20 -0400

    update extern docs
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/extern/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/extern/__init__.py
@@ -1,5 +1,25 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
+# -*- coding: utf-8 -*-
+# see LICENSE.rst
+
+"""External Python Packages.
+
 This packages contains python packages that are bundled with the package but
 are external to it, and hence are developed in a separate source tree.
+
 """
+
+# __credits__ = [""]
+# __license__ = ""
+
+
+# __all__ = [
+#     ""
+# ]
+
+
+##############################################################################
+# IMPORTS
+
+
+##############################################################################
+# END
